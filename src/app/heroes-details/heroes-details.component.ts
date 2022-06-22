d2e0078dc099bcfa01064878143fb13ba8b8ffd1
Kurt Ian Khalid I. Israel <kiisrael@up.edu.ph>
2022-06-22 21:06:06 +0800

    part 3 of tutorial is done

---
+++ src/app/heroes-details/heroes-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../interfaces/hero';
+
+@Component({
+  selector: 'app-heroes-details',
+  templateUrl: './heroes-details.component.html',
+  styleUrls: ['./heroes-details.component.css']
+})
+export class HeroesDetailsComponent implements OnInit {
+
+  @Input() hero? : Hero;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
